1ed5e5a34729df3253b3e26ea9303d3f6c37961d
Ilya-Zhirukhin <99948155+Ilya-Zhirukhin@users.noreply.github.com>
2023-02-28 17:15:19 +0300

    Update linear_regression_model.py

--- models/linear_regression_model.py
+++ models/linear_regression_model.py
@@ -3,7 +3,7 @@ import numpy as np
 class LinearRegression():
 
     def __init__(self, base_functions: list):
-        self.weights = None # TODO: init weights using np.random.randn (normal distribution with mean=0 and variance=1)
+        self.weights = np.random.randn(len(base_functions)) # init weights using np.random.randn
         self.base_functions = base_functions
 
     @staticmethod
@@ -12,16 +12,23 @@ class LinearRegression():
         pass
 
     def __plan_matrix(self, inputs: np.ndarray) -> np.ndarray:
-        # TODO build Plan matrix using list of lambda functions defined in config. Use only one loop (for base_functions)
-        pass
+        """build Plan matrix using list of lambda functions defined in config. Use only one loop (for base_functions)"""
+        n = len(inputs)
+        p = len(self.base_functions)
+        plan_matrix = np.zeros((n, p))
+
+        for i in range(p):
+            plan_matrix[:, i] = self.base_functions[i](inputs)
+
+        return plan_matrix
 
     def __calculate_weights(self, pseudoinverse_plan_matrix: np.ndarray, targets: np.ndarray) -> None:
         """calculate weights of the model using formula from the lecture. Not this homework"""
         pass
 
     def calculate_model_prediction(self, plan_matrix) -> np.ndarray:
-        # TODO calculate prediction of the model (y) using formula from the lecture
-        pass
+        """calculate prediction of the model (y) using formula from the lecture"""
+        return plan_matrix @ self.weights
 
     def train_model(self, inputs: np.ndarray, targets: np.ndarray) -> None:
         """Not this homework"""
@@ -33,3 +40,4 @@ class LinearRegression():
         predictions = self.calculate_model_prediction(plan_matrix)
 
         return predictions
+
