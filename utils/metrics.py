f6bdbcf3a19c73d888aa284402f45addc40470a5
ilya <99948155+Ilya-Zhirukhin@users.noreply.github.com>
2023-04-04 16:49:32 +0300

    Delete utils directory

--- utils/metrics.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def MSE(predictions: np.ndarray, targets: np.ndarray) -> float:
-    """Calculates the mean squared error between predictions and targets."""
-    return np.mean((predictions - targets) ** 2)
-
